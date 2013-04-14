c9d196954aefabc19c807e8269390f634aae02d5
Tori Breitkopf <brightcough@gmail.com>
2013-04-14 00:40:14 -0400

    Add Bootstrap

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source 'https://rubygems.org'
+ source 'https://rubygems.org'
 
 gem 'rails', '3.2.12'
 gem 'jquery-rails'
@@ -14,6 +14,6 @@ end
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-
   gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Omrails::Application.routes.draw do
+  
+get 'about' => 'pages#about'
+  root :to => 'pages#home' 
+
+  
+end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +62,4 @@ Omrails::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
